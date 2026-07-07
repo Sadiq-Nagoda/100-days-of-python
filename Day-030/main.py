2681a1609cbceee915c3ff16761123f655729293
Sadiq Usman Nagoda <sadiqnagoda1848@gmail.com>
2026-07-07 17:01:26 +0100

    Day 30: Complete project and final improvements

---
+++ Day-030/main.py
@@ -0,0 +1,12 @@
+# #FileNotFound
+# try:
+#     file = open("a_file.txt")
+#     a_dict = {"key : value"}
+#     print(a_dict["dbnscjiw"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "a")
+#     file.write("some thing")
+# except TypeError as error_message:
+# #     print("The key doesnt exist")
+
+
